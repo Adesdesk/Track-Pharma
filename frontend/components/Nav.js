d66a15bd5fe5e03209133aa181113f1041460ea3
Adesdesk <aadelakun28@gmail.com>
2023-01-23 23:32:49 +0100

    bring branch up to date

---
+++ frontend/components/Nav.js
@@ -0,0 +1,52 @@
+import Image from 'next/image'
+import React from 'react'
+import logo from '../public/logo1-1.png'
+import { HiOutlineMoon } from "react-icons/hi";
+import MobileMenu from './MobileMenu';
+
+import { ConnectKitButton } from "connectkit";
+import Link from 'next/link';
+
+function Nav() {
+
+    return (
+        <div className='w-full h-24 bg-pharmaGreen-800'>
+            <div className='flex justify-between items-center p-4 md:w-10/12 md:mx-auto'>
+                <div>
+                    <Image className='w-40 cursor-pointer' src={logo} alt="logo" />
+                    <p className='flex justify-end text-xs text-white font-normal cursor-pointer'>By Team201</p>
+                </div>
+                <div className='hidden md:flex'>
+                    <ul className='text-white flex space-x-4'>
+                        <li className='font-normal p-1 cursor-pointer transition ease-linear duration-150 text-orange-400'>
+                            <Link href='/'>Home</Link>
+                        </li>
+                        <li className='font-normal p-1 cursor-pointer'>
+                            <Link href='/products'>Products</Link>
+                        </li>
+                        <li className='font-normal p-1 cursor-pointer'>
+                            <Link href='/users'>Users</Link>
+                        </li>
+                        {/*<li className='font-normal p-1 cursor-pointer'>Contact</li>*/}
+                    </ul>
+                    <div className='ml-3'>
+                        <ConnectKitButton />
+                    </div>
+                </div>
+                <div className='md:hidden'>
+                    <div className='flex items-center space-x-2 text-white'>
+                        <div className='p-1 rounded-lg border border-pharmaGreen-700'>
+                            <HiOutlineMoon className='text-pharmaGreen-700 cursor-pointer' size={21} />
+                        </div>
+                        <div className='p-1 rounded-lg border border-pharmaGreen-700'>
+                            <ConnectKitButton />
+                        </div>
+                        <MobileMenu />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Nav
---
+++ frontend/node_modules/connectkit/build/components/ConnectModal/ConnectWithInjector/SquircleSpinner/index.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="react" />
+declare const SquircleSpinner: ({ logo, connecting, countdown, }: {
+    logo?: React.ReactNode;
+    connecting?: boolean | undefined;
+    countdown?: boolean | undefined;
+}) => JSX.Element;
+export default SquircleSpinner;
