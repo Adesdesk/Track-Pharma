0d2ecb216f6b6db0bbcb1bf17c0c65d073f7a685
Adesdesk <aadelakun28@gmail.com>
2023-01-23 21:52:45 +0100

    Update Nav.js

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -14,7 +14,7 @@ function Nav() {
             <div className='flex justify-between items-center p-4 md:w-10/12 md:mx-auto'>
                 <div>
                     <Image className='w-40 cursor-pointer' src={logo} alt="logo" />
-                    {/*<h6 className='font-normal p-1 cursor-pointer'>By Team201</h6>*/}
+                    <p className='text-sm text-white font-normal pl-2 cursor-pointer'>By Team201</p>
                 </div>
                 <div className='hidden md:flex'>
                     <ul className='text-white flex space-x-4'>
