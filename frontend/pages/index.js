5caafa866e04189e9fece99f52e9fed30fa991f7
Adesdesk <aadelakun28@gmail.com>
2023-01-24 12:52:44 +0100

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -23,7 +23,10 @@ export default function Home() {
             <p className='text-white mt-8 mb-16'>
               Empowering healthcare through decentralization: the future of pharmaceutical supply chain management is here!
             </p>
-            <Link href='/products' className='py-4 px-6 text-white font-bold text-2xl rounded-md bg-gradient-to-br from-pharmaGreen-700 to-yellow-500'>Explore Products</Link>
+            {/*<Link href='/products' className='py-4 px-6 text-white font-bold text-2xl rounded-md bg-gradient-to-br from-pharmaGreen-700 to-yellow-500'>Explore Products</Link>*/}
+            <Link href='/products' className='block py-4 px-6 text-white font-bold text-2xl 
+            rounded-md bg-gradient-to-br from-pharmaGreen-700 to-yellow-500 md:text-3xl lg:text-4xl 
+            xl:text-5xl md:py-6 lg:py-8 xl:py-10 md:px-8 lg:px-10 xl:px-12'>Explore Products</Link>
             </div>
           </div>
         </div>
